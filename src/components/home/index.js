5f72fb75dcf7ce91eeaf29139841f08a73a3b96d
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2019-08-20 21:33:30 -0700

    Added fetch request

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Form, Button } from 'react-bootstrap';
 
 class Home extends Component {
   render() {
@@ -7,15 +7,39 @@ class Home extends Component {
       <div className="Home">
         <Jumbotron>
           <h1>
-            Welcome!
+            It is time to do something.
           </h1>
 
+          <br></br>
+
           <p>
-            It is time to do something.
+            Enter your zip code to see who your local representatives are and if
+            they have received money from the NRA, and if so how much.
           </p>
+
+          <br></br>
+          
+          <Form onSubmit={this.getReps}>
+            <Form.Group controlId="formSearch">
+              <Form.Control type="text" placeholder="Enter zip code"/>
+              <br></br>
+              <Button type="submit">Search</Button>
+            </Form.Group>
+          </Form>
         </Jumbotron>
       </div>
     );
+  }
+
+  getReps() {
+    let request = process.env.REACT_APP_REP_ENDPOINT
+    fetch(request, {mode: 'no-cors'})
+      .then(function(response) {
+        console.log(response); 
+      })
+      .catch(function(error) {  
+        console.log('Request failed', error)  
+      });
   }
 }
 
